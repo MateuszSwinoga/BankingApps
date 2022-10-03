b3d67ee9c32f5e95f0e58f3a8140aae1dd4ba5c5
Mateusz Świnoga <mateusz19111@gmail.com>
2022-10-03 20:40:45 +0200

    Added loop and option exit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,40 +13,46 @@ public class Main {
         String name = sc.next();
         System.out.println("Please, enter your surname: ");
         String surname = sc.next();
+        int select;
 
 
     Account user=new Account(name, surname);
 
-        System.out.println("Select operation:\n1.User details\n2.Deposit money\n3.Withdraw money");
-        int select = sc.nextInt();
+    do {
+            System.out.println("Select operation:\n1.User details\n2.Deposit money\n3.Withdraw money\n4.Exit program");
+           select = sc.nextInt();
+            switch (select) {
 
-        switch (select) {
+                case 1:
+                    System.out.println("User details: ");
+                    System.out.println(user.details());
+                    break;
+                case 2:
+                    System.out.println("Deposit money: ");
+                    System.out.println("How much money do you want to deposit?");
+                    int deposit = sc.nextInt();
+                    user.deposit(deposit);  // aktualizacja/podmina danych w klasie Account
+                    System.out.println("Money after deposit:" + user.getBalance());
+                    break;
+                case 3:
+                    System.out.println("Withdraw money: ");
+                    System.out.println("How much do you want to withdraw?");
+                    int withdraw = sc.nextInt();
+                    if (user.getBalance() >= withdraw) {
+                        user.withdraw(withdraw); // aktualizacja/podmina danych w klasie Account
+                        System.out.println("Money after withdraw: " + user.getBalance());
+                    } else {
+                        System.out.println("We cant do this operation becouse your account balance is under withdraw value");
+                    }
 
-            case 1:
-                System.out.println("User details: ");
-                System.out.println(user.details());
-                break;
-            case 2:
-                System.out.println("Deposit money: ");
-                System.out.println("How much money do you want to deposit?");
-                int deposit = sc.nextInt();
-                user.deposit(deposit);  // aktualizacja/podmina danych w klasie Account
-                System.out.println("Money after deposit:" + user.getBalance());
-                break;
-            case 3:
-                System.out.println("Withdraw money: ");
-                System.out.println("How much do you want to withdraw?");
-                int withdraw = sc.nextInt();
-                if (user.getBalance()>=withdraw){
-                    user.withdraw(withdraw); // aktualizacja/podmina danych w klasie Account
-                    System.out.println("Money after withdraw: " + user.getBalance());
-                }
-                else {
-                    System.out.println("We cant do this operation becouse your account balance is under withdraw value");
-                }
+                    break;
+                case 4:
+                    System.out.println("Exit program");
 
-                break;
 
+            }
         }
+        while (select!=4);
+
     }
 }
